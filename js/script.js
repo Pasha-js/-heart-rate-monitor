b3aff278b2ff896a3be66cce778cf107f65cc46c
Pasha <Pashakhurtenko@gmail.com>
2020-09-08 19:21:10 +0300

    another file

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 700,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>'
+      });
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content'). removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      
+      $('.catalog-item__link').each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();  
+          $('.catalog_item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      })
+  }); 
